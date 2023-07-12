8d1ea6d61a9eb8304b14d64bc1e24dd68b4be9be
Cosmin <cosma.cosmin@emeal.nttdata.com>
2023-07-12 12:49:17 +0300

    sync cu github vlad

---
+++ SearchTheWebServer/Validators/EmailValidator.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace SearchTheWebServer.Validators
+{
+    public static class EmailValidator
+    {
+        public static bool EmailPatternValidation(string? emailInput)
+        {
+            var emailformat = new Regex("^[A-Za-z0-9._%+-]+@[a-z.-]+.(com|ro)$");
+            var isValidated = emailformat.IsMatch(emailInput);
+
+            if (isValidated) return true;
+            else return false;      
+        }
+    }
+}
